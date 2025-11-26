37009a5f9b44df9fc719d63f94344be0d60d5873
Alejandro Cerezo <alce65@hotmail.es>
2025-11-26 10:03:29 +0100

    Add routes and use them in menu component

--- ng21/projects/demo1/src/app/app.ts
+++ ng21/projects/demo1/src/app/app.ts
@@ -2,19 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './core/components/menu/menu';
 import { Layout } from './core/components/layout/layout';
-import HomePage from './features/home/home-page';
-import AboutPage from './features/about/about-page';
-import ProofsPage from "./features/proofs/proofs-page";
+
 @Component({
   selector: 'ind-root',
-  imports: [RouterOutlet, Menu, Layout, HomePage, AboutPage, ProofsPage],
+  imports: [RouterOutlet, Menu, Layout],
   template: `
     <ind-layout class="layout">
-      <ind-menu class="main-menu"/>
+      <ind-menu class="main-menu" />
       <router-outlet />
-      <ind-home-page />
-      <ind-about-page />
-      <ind-proofs-page />
     </ind-layout>
   `,
   styles: [],
--- ng21/projects/demo1/src/app/core/components/menu/menu.ts
+++ ng21/projects/demo1/src/app/core/components/menu/menu.ts
@@ -1,40 +1,41 @@
 import { Component } from '@angular/core';
 import { MenuOption } from '../../types/menu-option';
+import { RouterModule } from '@angular/router';
+import { menuOptions } from '../../../app.routes';
 
 @Component({
   selector: 'ind-menu',
-  imports: [],
+  imports: [RouterModule],
   template: `
     <nav>
       <ul>
-        @for (item of menuOptions; track item.path) {
+        @for (item of options; track item.path) {
           <li>
-            <a [href]="item.path">{{ item.label }}</a>
+            <a [routerLink]="item.path" routerLinkActive="active">{{ item.label }}</a>
           </li>
         }
       </ul>
     </nav>
   `,
   styles: `
-    nav {
-      ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        gap: 1rem;
-      }
-      a {
-        color: inherit;
-        text-decoration: none;
-        font-weight: bold;
-      }
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      gap: 1rem;
+    }
+    a {
+      color: inherit;
+      text-decoration: none;
+      font-weight: bold;
+    }
+    .active {
+      text-decoration: underline;
+      color: #102337ff;
     }
   `,
 })
 export class Menu {
-  protected menuOptions: MenuOption[] = [
-    { label: 'Home', path: '/home' },
-    { label: 'About', path: '/about' },
-  ];
+  protected options: MenuOption[] = menuOptions;
 }
